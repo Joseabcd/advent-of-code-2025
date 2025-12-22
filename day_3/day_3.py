12a821185fd80863a61604adcf279721c6ee4022
Jose Antonio Fernandez Vicente <jose.a.f.alm@gmail.com>
2025-12-22 05:00:29 +0100

    Add solution for day 3 part 2.

--- day_3/day_3.py
+++ day_3/day_3.py
@@ -35,3 +35,22 @@ print(total_jolts)
 # Part 2
 # ------------------------------------------------------------
 
+k = 12
+total_jolts = 0
+for bank in bank_list:
+  plist = []
+  pstart = -1
+  i = 0
+  while i < k:
+    pstart += 1
+    plist += [pstart]
+    for p in range(pstart + 1, len(bank) - (k - 1) + i):
+      if bank[plist[i]] == '9':
+        break
+      if bank[p] > bank[plist[i]]:
+        plist[i] = p
+        pstart = p
+    i += 1
+  total_jolts += int(''.join(bank[elem] for elem in plist))
+
+print(total_jolts)
